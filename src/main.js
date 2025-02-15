3b73aa5642ff7410b304f476cff94ce8a50a461c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-15 20:16:25 -0300

    inicio do desenvolvimento da funcionalidade de pegar os dados do formulario.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const main = document.querySelector("#main");
+const button = document.querySelector("#button");
+const content = document.querySelector("#content");
+const labels = document.querySelectorAll("label");
+const inptus = document.querySelectorAll("input");
+
+
+button.addEventListener("click",() => {
+    alert("teste");
+});
